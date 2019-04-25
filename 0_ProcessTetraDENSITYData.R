334c30ae094a993537a67bac037d4b7b828abc37
Tim Newbold <timnewbold.research@gmail.com>
2019-04-25 09:50:33 +0100

    Removed redundant code referring to old version of TetraDENSITY

--- 0_ProcessTetraDENSITYData.R
+++ 0_ProcessTetraDENSITYData.R
@@ -3,7 +3,6 @@ dataDir <- "0_data/"
 outDir <- "0_ProcessTetraDENSITYData/"
 
 tetraDens <- read.csv(paste0(dataDir,"TetraDENSITY.csv"))
-tetraDensOrig <- read.csv(paste0(dataDir,"AverageDensities.csv"))
 
 tetraDens$Species <- paste(tetraDens$Genus,tetraDens$Species)
 
@@ -11,11 +10,6 @@ avDens <- tapply(X = tetraDens$Density,
                  INDEX = tetraDens$Species,
                  FUN = function(x){
                    return(10^mean(log10(x)))})
-
-avDensOrig <- tetraDensOrig$Density_km2[match(
-  names(avDens),tetraDensOrig$Species)]
-
-print(cor.test(avDens,avDensOrig))
 
 classes <- tapply(X = paste(tetraDens$Class),
                   INDEX = tetraDens$Species,
